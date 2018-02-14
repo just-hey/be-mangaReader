903f62b774780e28c993529dc93bc3bafd88d4e2
Justin Hays <jhays124@yahoo.com>
2018-02-14 15:38:15 -0800

    possible refactor prep

--- src/controllers/chapters.js
+++ src/controllers/chapters.js
@@ -1,8 +1,9 @@
 const model = require('../models/chapters')
 
 const getChapter = (req, res, next) => {
-  console.log(req.params.id);
+  console.log(req.params.id)
   let response = model.getChapter(req.params.id)
+  console.log('we make it out?',response)
   res.send(200).json(response)
 }
 
--- src/models/chapters.js
+++ src/models/chapters.js
@@ -1,7 +1,11 @@
 const baseURL = 'www.mangaeden.com/api/manga/'
 
 const getChapter = (id) => {
-  return `${baseURL}${id}`
+  // console.log(id)
+  // axios.get(`${baseURL}${id}`)
+  //   .then(result => {
+  //     console.log('axios is returning...',result)
+  //   })
 }
 
 module.exports = { getChapter }
