0da8e4b2495d5d0896e0d954f8f430f796cc3c38
Justin Hays <jhays124@yahoo.com>
2018-02-16 10:53:45 -0800

    knex added

--- knexfile.js
+++ knexfile.js
@@ -13,5 +13,15 @@ module.exports = {
     seeds: {
       directory: path.join(__dirname, 'db', 'seeds')
     }
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
   }
 }
